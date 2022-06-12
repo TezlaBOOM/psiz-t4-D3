4bf7f69d233b581f39c626263a56e3bf066c5e11
Witold Michalak <Witold3152000@wp.pl>
2022-06-12 21:58:41 +0200

    0.2
    tworzenie pliku spawdzenia

--- psiz-t4.cpp
+++ psiz-t4.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <stdint.h>
+#include <vector>
+#include <bitset>
+#include <ctime>
+#include <iomanip>
+#include <chrono>
+#include <time.h>
 using namespace std;
+void Nowypliki(string nazwaPliku, int licznik, char wartosc)
+{
+    ofstream mFile(nazwaPliku);
+
+        for (int i = 0; i < licznik; i++) {
+            mFile << wartosc;
+
+        }
+}
+
 
 
 int main( int argc, char * argv[] )
 {
-    cout<< "Hello World!" <<endl;
+    Nowypliki("test1.bin", 100, 0x55);
+    Nowypliki("test2.bin", 100, 0x55);
 
+    Nowypliki("test3.bin", 100, 0x55);
+    Nowypliki("test4.bin", 100, 0xF5);
+
+    Nowypliki("test5.bin", 1024*1024*400, 0x55);
+    Nowypliki("test6.bin", 1024*1024*400, 0x50);
     return 0;
 }
