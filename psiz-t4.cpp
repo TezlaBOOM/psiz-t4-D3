3e09afec112dd1422a3005583ceb642699073742
Witold Michalak <Witold3152000@wp.pl>
2022-06-14 22:07:13 +0200

    1.0
    program działa

--- psiz-t4.cpp
+++ psiz-t4.cpp
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include <chrono>
 #include <time.h>
+#include <math.h>
 using namespace std;
 void Nowypliki(string nazwaPliku, int licznik, char wartosc)
 {
@@ -18,26 +19,81 @@ void Nowypliki(string nazwaPliku, int licznik, char wartosc)
 
         }
 }
-void log(int bit, int ber){
 
-
+void logtxt(const char* Plik1,const char* Plik2, float bit, float ber){
+    double wynikber;
+    wynikber = ber / bit;
     ofstream mFile("log.txt", ios::app);
-    mFile << "bit: " << bit << " ber: " << ber << endl;
+    mFile <<"Porównaie pliku:"<< Plik1<< " i "<< Plik2 << " wynosi w bitach: " << bit << " i w ber: " << wynikber << endl;
+    cout <<"Porównaie pliku:"<< Plik1<< " i "<< Plik2 << " wynosi w bitach: " << bit << " i w ber: " << wynikber << endl;
+}
+
+
+uint8_t licz(uint8_t a, uint8_t b)
+{
+    uint8_t c = a ^ b; 
+    uint8_t setBits = 0;
+    while (c > 0)
+    {
+        setBits += c & 1;
+        c >>= 1;
+    }
+    return setBits;
 }
 
 
 
-int main( int argc, char * argv[] )
+void Porownanie(const char* Plik1,const char* Plik2) {
+    ifstream file1 (Plik1, ios::binary);
+    ifstream file2 (Plik2, ios::binary);
+    char a{};
+    char b{};
+    float  bit = 0, ber = 0;
+
+    while (!file1.eof())
+    {
+        file1 >> a;
+        file2 >> b;
+
+        if (file1.eof()) { break; }
+
+        bit = 8 + bit;
+        ber = licz(a, b) + ber;
+      
+
+    }
+
+    logtxt(Plik1,Plik2,bit,ber);
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
 {
-    log(1,2);
+    
 
-   /* Nowypliki("test1.bin", 100, 0x55);
+   Nowypliki("test1.bin", 100, 0x55);
     Nowypliki("test2.bin", 100, 0x55);
-
-    Nowypliki("test3.bin", 100, 0x55);
+    Porownanie("test1.bin","test2.bin");
+    
+  Nowypliki("test3.bin", 100, 0x55);
     Nowypliki("test4.bin", 100, 0xF5);
-
+    Porownanie("test3.bin","test4.bin");
+/*
     Nowypliki("test5.bin", 1024*1024*400, 0x55);
-    Nowypliki("test6.bin", 1024*1024*400, 0x50);*/
+    Nowypliki("test6.bin", 1024*1024*400, 0x50);
+    Porownanie("test5.bin","test6.bin");*/
     return 0;
 }
